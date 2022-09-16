c6bb01d4e9532016c6c60387290169c2e2601f41
Ilya Levshenkov <ivl1969@mail.ru>
2022-09-16 11:08:52 +0300

    Ex25Less4

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Первая программа в VS
+
+
+
+Console.WriteLine("Hello LevshiK");
+
+string name = "Ilya";
+int age = 53;
+double weight = 81.2;
+object name2 = "Levshik numer 30";
+bool allive = true;
+
+Console.WriteLine($"Имя: {name}");
+Console.WriteLine($"Возаст:  {age}");
+Console.WriteLine($"Вес: {weight}");
+Console.WriteLine($"Имя: {allive}");
+
+Console.Write($"Имя: {name}  Возаст:  { age}  Вес: { weight}    Имя: { allive}");
